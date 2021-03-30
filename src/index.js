e9b25005b446414dee0fafd7d912f485431a8a0c
Davi Sousa <devsousadavi@gmail.com>
2021-03-30 18:26:08 -0300

    commit inicial

---
+++ src/index.js
@@ -0,0 +1,42 @@
+const { request, response } = require("express");
+const express = require("express");
+const { v4: uuidv4 } = require('uuid')
+const app = express();
+
+app.use(express.json());
+
+
+const customers = []
+
+app.post('/account', (request, response) => {
+  const { cpf, name } = request.body;
+
+
+  const customerAlreadyExixts = customers.some((customer) => customer.cpf === cpf)
+
+  if (customerAlreadyExixts) {
+    return response.status(400).json({ error: "customer already exists" })
+  }
+  customers.push({
+    cpf, name, id: uuidv4(), statement: []
+  })
+
+
+  return response.status(201).json(customers)
+
+})
+
+app.get('/account', (request, response) => {
+
+  return response.json(customers)
+})
+
+app.get("/statement/:cpf", (request, response) => {
+  const { cpf } = request.params;
+
+  const customer = customers.find(customer => customer.cpf === cpf);
+
+  return response.json(customer.statement);
+})
+
+app.listen(3333, () => 'rodou')
